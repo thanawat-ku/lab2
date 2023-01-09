6d345d3a038392223d7726f26d61240c3d87340d
Thanawat P <thanawat.pha@ku.th>
2023-01-09 12:06:45 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="show.php" method="post">
+        What is your name:<input type="text" name="name"><br>
+        What is your age:<input type="text" name="age"><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
